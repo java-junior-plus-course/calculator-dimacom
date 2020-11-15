952657c8049f4d82ccfadae3e49abdce0e614b44
dima <karpov83d@gmail.com>
2020-11-15 21:07:12 +0300

    create new methods add , divvy, minus , multi.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Calculator {
+
+
+public class Calculator {
+    private double operandOne;
+    private double operandTwo;
+
+    public Calculator() {
+    }
+
+    public Calculator(double operandOne, double operandTwo) {
+        this.operandOne = operandOne;
+        this.operandTwo = operandTwo;
+
+    }
+
+    public double add() {
+        return operandOne + operandTwo;
+    }
+
+    public double minus() {
+        return operandOne - operandTwo;
+    }
+
+    public double multi() {
+        return operandOne * operandTwo;
+    }
+
+    public double divvy() {
+        return operandOne / operandTwo;
+    }
+
+    public double getOperandOne() {
+        return operandOne;
+    }
+
+    public void setOperandOne(double operandOne) {
+        this.operandOne = operandOne;
+    }
+
+    public double getOperandTwo() {
+        return operandTwo;
+    }
+
+    public void setOperandTwo(double operandTwo) {
+        this.operandTwo = operandTwo;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Calculator count = new Calculator();
+        count.setOperandOne(7.6);
+        count.setOperandTwo(3);
+        System.out.println(count.multi());
+        System.out.println(count.add());
+        System.out.println(count.divvy());
+        System.out.println(count.minus());
+
     }
+
 }
